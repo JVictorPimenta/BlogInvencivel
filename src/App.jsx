7223f8c5524ceea1b9c61b81e0b01a9d0cc2bdbc
João Victor Pimenta <joaovictorp.r77@gmail.com>
2025-06-02 10:20:03 -0300

    Criação das páginas e conexão

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import PostsList from './components/PostsList'
+import PostDetails from './components/PostDetails'
 import './App.css'
 
 function App() {
-
-  return (abobora)
+  return (
+    <div className="App">
+      <h1>Blog Posts</h1>
+      <Routes>
+        <Route path="/" element={<PostsList />} />
+        <Route path="/post/:id" element={<PostDetails />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
